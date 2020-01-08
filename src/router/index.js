386030675ad32dfd51f4579332b736a935bbcd51
cyygith <770764225@qq.com>
2020-01-08 17:29:05 +0800

    accbook提交

--- src/router/index.js
+++ src/router/index.js
@@ -65,9 +65,13 @@ let router = new Router({
         {path: '/cArticleManager', component: resolve => require(['../components/page/contents/article/cArticleManager.vue'], resolve), meta: { title: '文章管理' }},
         //报表管理
         {path: '/goodsLineChart', component: resolve => require(['../components/page/contents/chart/goodsLineChart.vue'], resolve), meta: { title: '商品线形图' }},
+        //记账管理
+        {path: '/accBookList', component: resolve => require(['../components/page/account/accbook/accBookList.vue'], resolve), meta: { title: '记账管理' }},
+        {path: '/accBookManager', component: resolve => require(['../components/page/account/accbook/accBookManager.vue'], resolve), meta: { title: '记账管理' }},
         //代码生成器
         {path: '/toolGenCodeList', component: resolve => require(['../components/page/tool/gen/toolGenCodeList.vue'], resolve), meta: { title: '代码生成器' }},
         {path: '/toolGenCodeManager', component: resolve => require(['../components/page/tool/gen/toolGenCodeManager.vue'], resolve), meta: { title: '代码生成器' }},
+      
       ]
     },
     {
